e6eb06e649b3261ea089e7b7f1e3bd6c013c0f05
sitdi <sitdikov.88@inbox.ru>
2022-06-23 20:03:17 +0300

    dragAndDropTo

---
+++ src/test/java/github/DragAndDrop.java
@@ -0,0 +1,30 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+
+
+
+     @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1400x760";
+        Configuration.browserPosition = "-10x0"; // экран был не по центру
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.browser = "firefox";
+    }
+    @Test
+    void dragAndDrop() {
+
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
+
